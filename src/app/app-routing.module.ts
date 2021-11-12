8e7480514f225bddab8659bf55ea1cd1ffc7341e
Yuta Miyoshi <miyoshi581@gmail.com>
2021-11-12 10:18:50 +0900

    テスト用のレイアウトを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Layout1Component } from './layout1/layout1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'layout1',
+    component: Layout1Component,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Layout1Component } from './layout1/layout1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Layout1Component
   ],
   imports: [
     BrowserModule,
