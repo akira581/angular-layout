5aa616b0009b580e3bbac932ffe224ccd009da1c
Yuta Miyoshi <miyoshi581@gmail.com>
2021-11-13 15:15:55 +0900

    ルーティング設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Layout1Component } from './layout1/layout1.component';
+import { Layout2Component } from './layout2/layout2.component';
+import { TopComponent } from './top/top.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: TopComponent,
+  },
+  {
     path: 'layout1',
     component: Layout1Component,
+  },
+  {
+    path: 'layout2',
+    component: Layout2Component,
   },
 ];
 
