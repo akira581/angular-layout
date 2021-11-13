eea9130de9853bebb3d8f6272509b027e9a8363e
Yuta Miyoshi <miyoshi581@gmail.com>
2021-11-13 15:11:31 +0900

    layout2追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Layout1Component } from './layout1/layout1.component';
+import { Layout2Component } from './layout2/layout2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Layout1Component
+    Layout1Component,
+    Layout2Component
   ],
   imports: [
     BrowserModule,
